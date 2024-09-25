9e1dfa8ecb16736a03bc2476ea45a2a6fab9605b
fishsoupp <limjingyu120@gmail.com>
2024-09-25 15:16:28 +0800

    href and home

--- app.py
+++ app.py
@@ -4,9 +4,27 @@ from admin.routes import adminRoutes
 app = Flask(__name__)
 app.register_blueprint(adminRoutes, url_prefix="/admin")
 
-@app.route('/')
-def hello_world():
+@app.route('/index')
+def index():
     return render_template("index.html")
+
+
+@app.route('/ranking')
+def ranking():
+    return render_template("ranking.html")
+
+@app.route('/teams')
+def teams():
+    return render_template("teams.html")
+
+@app.route('/players')
+def players():
+    return render_template("players.html")
+
+@app.route('/stats')
+def stats():
+    return render_template("statistics.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
