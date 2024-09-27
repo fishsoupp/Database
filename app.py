fca8c7c58b15031e3aaf80dfd4c2af880d9f3471
Kenixnge1011 <kenixnge1011@gmail.com>
2024-09-27 18:47:12 +0800

    connection from flask to pgadmin 4

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, redirect, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
 from admin.routes import adminRoutes
 
 app = Flask(__name__)
 app.register_blueprint(adminRoutes, url_prefix="/admin")
+
+# Configuration for PostgreSQL database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/TicketGrabdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize the database
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def hello_world():
